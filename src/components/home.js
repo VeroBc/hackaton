c1cc8ae63fdcd852b942db0264ae9d7bedeac335
Vero Bc <verobc.cel@gmail.com>
2021-01-26 09:59:11 -0500

    home

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Category } from './category';
+import { BrowserRouter as Link } from "react-router-dom";
 import '../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 export const Home = () => {
 
@@ -9,6 +12,13 @@ export const Home = () => {
   return (
     <div>
       <section >
+        <nav className='navbar'>
+          {/* <img src={logo} className='imglogo' alt="Logo"/> */}
+          <a href="/about" className='signup'><Link to="/about">Nosotros</Link></a>
+          <a href="/signup" className='signup'><Link to="/signup">Regístrate</Link></a>
+          <a href="/signin" className='signin'><Link to="/signin">Inicia sesión</Link></a>          
+        </nav>
+
         <div className='subheader'>
           <h1 className='filters'>
             <a href="#" onClick={()=> setCurrentcategory('maquillaje')}>Maquillaje</a>
@@ -17,6 +27,8 @@ export const Home = () => {
             <a href="#" onClick={()=> setCurrentcategory('cuidado personal')}>Cuidado personal</a>
             <a href="#" onClick={()=> setCurrentcategory('tratamiento corporal')}>Tratamiento corporal</a>
             <a href="#" onClick={()=> setCurrentcategory('tratamiento facial')}>Tratamiento facial</a>
+            <FontAwesomeIcon className ='icon' icon={faShoppingCart} />
+          
           </h1>
         </div>
         <div>
