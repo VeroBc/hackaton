c20e5e7e0dcc35157b701364c65e1b4a64a4d163
Vero Bc <verobc.cel@gmail.com>
2021-01-26 13:13:21 -0500

    router header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Home } from './components/home';
 import { Nosotros } from './components/nosotros';
 import { Marcas } from './components/nuestrasMarcas';
-import { Category } from './components/category';
+import { SelectCategory } from './components/selectCategory';
 import { Promociones } from './components/promociones';
 import { Contacto } from './components/contacto';
-// import { Restaurant } from './components/restaurant';
 import { Signup } from './components/signup';
 import { Signin } from './components/signin';
-import { About } from './components/about';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faInstagram } from "@fortawesome/fontawesome-free-brands";
+import {faFacebookSquare } from "@fortawesome/fontawesome-free-brands";
+import {faWhatsapp } from "@fortawesome/fontawesome-free-brands";
+import {faYoutube } from "@fortawesome/fontawesome-free-brands";
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 import './services/firebase';
 import {
@@ -23,23 +28,27 @@ export const App = () => {
   return (
     <Router>
       <div>
+      <div className="icon-container">
+        <FontAwesomeIcon className= "icons-redes"icon={faFacebookSquare}/>{" "}
+        <FontAwesomeIcon className= "icons-redes"icon={faInstagram}/>{" "}
+        <FontAwesomeIcon className= "icons-redes"icon={faWhatsapp}/>{" "}
+        <FontAwesomeIcon className= "icons-redes"icon={faYoutube}/>{" "}
+      </div> 
         <nav>
-           <Link to="/">Home</Link>
+            <Link to="/">Home</Link>
             <Link to="/nosotros">Nosotros</Link>
             <Link to="/nuestrasMarcas">Nuestras Marcas</Link>
             <Link to="/categorias">Categorias</Link>
             <Link to="/promociones">Promociones</Link>
             <Link to="/contacto">Contacto</Link>
+            <Link to="/signin"><FontAwesomeIcon className ='icon' icon={faUser} /></Link>
+            <Link to="/cart"><FontAwesomeIcon className ='icon' icon={faShoppingCart} /></Link>
+            
         </nav>
         <Switch>
           <Route exact path="/">
-          <Route path="/about">
-            <About/>
-          </Route>
-          <Route path="/">
-
-            <Home />
-            
+            <Home/>
+            <SelectCategory />
           </Route>
           <Route path="/nosotros">
             <Nosotros />
@@ -48,7 +57,7 @@ export const App = () => {
             <Marcas />
           </Route>
           <Route path="/categorias">
-            <Category />
+            <SelectCategory />
           </Route>
           <Route path="/promociones">
             <Promociones />
@@ -62,6 +71,7 @@ export const App = () => {
           <Route path="/signup">
             <Signup />
           </Route>
+  
         </Switch>
       </div>
     </Router>
