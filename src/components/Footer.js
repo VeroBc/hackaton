aaac0b204af4e9f3e376cafb069e003612e127f4
Vero Bc <verobc.cel@gmail.com>
2021-01-28 02:37:14 -0500

    after merging

---
+++ src/components/Footer.js
@@ -0,0 +1,58 @@
+import "./Footer.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faInstagram, faFacebookSquare, faYoutube } from "@fortawesome/fontawesome-free-brands";
+import {
+  Link
+} from "react-router-dom";
+
+export const Footer = () => {
+  return (
+    <div>
+      <div className="footer-container">
+          <div> <img src="https://i.ibb.co/8gDCgXw/logo-ultimo.jpg" alt="logo-ultimo" border="0" className= "logo"></img></div>
+         
+           
+        <div>
+          <ul>
+            <li>L´bel</li>
+            <li>Ésika</li>
+            <li>Cyzone</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>Terminos y condiones</li>
+            <li>Condiciones de uso web</li>
+            <li>Quiz Capevedi</li>
+            <li>Contáctanos</li>
+          </ul>
+          </div>
+
+          <div>
+          <ul>
+            <li>Política de privacidad y cookies</li>
+            <li>Codigo de etica Beautycorp</li>
+            <li>Pedidos y Devoluciones</li>
+            <li>Búsqueda Avanzada</li>
+          </ul>
+        </div>
+        <div className= "icon-container">
+          <div>
+          <a href="https://www.instagram.com/belcorpcorporativo/?hl=es-la"className="router-icon"><FontAwesomeIcon className="router-icon" icon={faInstagram} /></a>
+          <a href="https://www.facebook.com/BelcorpCorporativo/"className="router-icon"><FontAwesomeIcon className ='router-icon' icon={faFacebookSquare} /></a>
+          <a href="https://www.youtube.com/user/somosbelcorp"className="router-icon"><FontAwesomeIcon className ='router-icon' icon={faYoutube} /></a>
+            </div>
+            </div>
+
+</div>
+<div className="cont-img-tarj"><img src="https://i.ibb.co/hmQvjmc/tarjetas.jpg" alt="tarjetas" border="0"className="img-tarjetas"></img></div>
+
+      <div className="footer-div">
+      <footer><h4 className="h4-footer">© 2020 Beautycorp Todos los derechos reservados</h4></footer>
+      </div>
+      
+    </div>
+  );
+};
--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,66 @@
 import '../App.css';
-import ReactPlayer from 'react-player'
+import React from 'react';
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+// import Button from 'react-bootstrap/Button';
 
+const fadeImages = [
+    'imagenes/banner1.png',
+    'imagenes/banner2.jpeg',
+    'imagenes/banner3.png'
+  ];
 export const Home = () => {
-    return (
-        <ReactPlayer url="https://youtu.be/luai0p0y2zE"/>
-    );
+
+    return ( 
+        // <div>
+        // <h2>Home</h2>
+        //     <img  alt="marca" className="mySlides" src="/imagenes/banner1.png"/>
+        //     <img  alt="marca" className="mySlides" src="/imagenes/banner1.png"/>
+        //     {/* <img class="mySlides" src="/imagenes/Banner.png"/> */}
+        //     <button className="w3-button w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+        //     <button className="w3-button w3-display-right" onclick="plusDivs(+1)">&#10095;</button>
+        // </div>
+        <div className="slide-container">
+        <Fade>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={fadeImages[0]} alt="marca"className="banner1  "/>
+            </div>
+          </div>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={fadeImages[1]} alt="marca" className="banner2"/>
+            </div>
+          </div>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={fadeImages[2]} alt="marca" className="banner3"/>
+            </div>
+          </div>
+        </Fade>
+        {/* <div  className= "container-bold">
+            <p>hola</p>
+        </div> */}
+        <img src="/imagenes/barra.png" alt="marca" className="container-bold"/>
+        <div  className= "container-texto">
+        <h1 className= "container-texto-proximos"> Próximos lanzamientos </h1>
+        </div>
+        <div className= "container-productos">
+            <img src="/imagenes/producto1.png" alt="marca" className="producto-home"/>
+            <img src="/imagenes/producto2.png" alt="marca" className="producto-home"/>
+            <img src="/imagenes/producto3.png" alt="marca" className="producto-home"/>           
+        </div>
+        <div className="grid-container">
+            <div className="foto1 img-hover-zoom--quick-zoom">
+            <img src="/imagenes/producto4.png" alt="marca" className="container-bold"/>
+            </div>
+            <div className="foto2 img-hover-zoom--zoom-n-pan-v">
+            <img src="/imagenes/producto6.png" alt="marca" className="container-bold"/>
+            </div>
+            <div className="foto3 img-hover-zoom--zoom-n-rotate ">
+            <img src="/imagenes/producto5.png" alt="marca" className="container-bold-foto3"/>
+            </div>
+        </div>
+      </div>
+    )
 }
